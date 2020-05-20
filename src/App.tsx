73f9480de77f2882948cd145a5be3238a4c79134
João Paulo <musicjoaopaulo@gmail.com>
2020-05-20 10:37:05 -0300

    chore(frontend): add baseUrl to the tsconfig json to fix root path

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
-import GlobalStyle from './styles/global';
+import GlobalStyle from 'styles/global';
 
 import Routes from './routes';
 
